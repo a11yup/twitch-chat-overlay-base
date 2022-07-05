b572d36b693ac95df52d86f1728a1d4467aaa7c4
A11y Up <a11yup+github@posteo.de>
2022-07-05 16:58:59 +0200

    Add handling of Twitch standard emotes (animated or static)

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const URL_PREFIX = `https://static-cdn.jtvnw.net/emoticons/v2`;
+const EMOTE_REGEX_PART_1 = "(?:^|(?<=\\s))";
+const EMOTE_REGEX_PART_2 = "(?:(?=\\s)|$)";
+
+const replaceEmotesWithImageTags = (message, emotes) => {
+  let result = message;
+
+  if (emotes) {
+    const replacementMap = {};
+
+    for (const [emoteId, occurenceIndices] of Object.entries(emotes)) {
+      const [startIndex, endIndex] = occurenceIndices[0].split("-");
+
+      const emoteText = message.slice(Number(startIndex), Number(endIndex) + 1);
+
+      replacementMap[
+        emoteText
+      ] = `<img src="${URL_PREFIX}/${emoteId}/default/dark/2.0" alt="emote" />`;
+    }
+
+    for (const [emoteText, replacement] of Object.entries(replacementMap)) {
+      const emotePattern = new RegExp(
+        `${EMOTE_REGEX_PART_1}${emoteText}${EMOTE_REGEX_PART_2}`,
+        "g"
+      );
+
+      result = result.replaceAll(emotePattern, replacement);
+    }
+  }
+
+  return result;
+};
+
 const MAX_MESSAGES_COUNT = 20;
 
 const client = tmi.Client({
@@ -7,14 +40,16 @@ const client = tmi.Client({
 client.connect();
 
 client.on("message", (channel, tags, message, self) => {
-  const userName = document.createElement("span");
-  userName.append(tags["display-name"]);
-  userName.style.color = tags.color;
-
   const messageElement = document.createElement("p");
 
-  messageElement.append(userName);
-  messageElement.append(`: ${message}`);
+  const userName = `<span style="color: ${tags.color}">${tags["display-name"]}</span>`;
+
+  const messageWithEmotesReplaced = replaceEmotesWithImageTags(
+    message,
+    tags.emotes
+  );
+
+  messageElement.innerHTML = `${userName}: ${messageWithEmotesReplaced}`;
 
   const chatBoxElement = document.querySelector(".chat-box");
   chatBoxElement.append(messageElement);
