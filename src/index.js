e33add8c371a5a4580dfcea0cad6db603a7e16d8
A11y Up <a11yup+github@posteo.de>
2022-07-08 02:02:41 +0200

    Fix bug with DOM not being ready

--- src/index.js
+++ src/index.js
@@ -1,47 +1,51 @@
 import createMessageElement from "./createMessageElement.js";
 
-const MAX_MESSAGES_COUNT = 20;
+function setup() {
+  const MAX_MESSAGES_COUNT = 20;
 
-const queryParameters = new URLSearchParams(window.location.search);
-const CHANNEL_NAME = queryParameters.get("channel");
-const CHAT_STRIP_MODE = queryParameters.has("chatStrip");
+  const queryParameters = new URLSearchParams(window.location.search);
+  const CHANNEL_NAME = queryParameters.get("channel");
+  const CHAT_STRIP_MODE = queryParameters.has("chatStrip");
 
-const client = tmi.Client({
-  channels: [CHANNEL_NAME],
-});
-
-client.connect();
-
-if (CHAT_STRIP_MODE) {
-  document.querySelector(".chat-box").style.display = "none";
-
-  client.on("message", async (channel, tags, message, self) => {
-    const messageElement = await createMessageElement(tags, message);
-
-    const chatBoxElement = document.querySelector(".chat-strip");
-    chatBoxElement.append(messageElement);
-
-    const allMessageElements = document.querySelectorAll(".chat-strip p");
-
-    if (allMessageElements.length > MAX_MESSAGES_COUNT) {
-      // remove the first message from the beginning of the list
-      allMessageElements[0]?.remove();
-    }
+  const client = tmi.Client({
+    channels: [CHANNEL_NAME],
   });
-} else {
-  document.querySelector(".chat-strip").style.display = "none";
 
-  client.on("message", async (channel, tags, message, self) => {
-    const messageElement = await createMessageElement(tags, message);
+  client.connect();
 
-    const chatBoxElement = document.querySelector(".chat-box");
-    chatBoxElement.append(messageElement);
+  if (CHAT_STRIP_MODE) {
+    document.querySelector(".chat-box").style.display = "none";
 
-    const allMessageElements = document.querySelectorAll(".chat-box p");
+    client.on("message", async (channel, tags, message, self) => {
+      const messageElement = await createMessageElement(tags, message);
 
-    if (allMessageElements.length > MAX_MESSAGES_COUNT) {
-      // remove the first message from the beginning of the list
-      allMessageElements[0]?.remove();
-    }
-  });
+      const chatBoxElement = document.querySelector(".chat-strip");
+      chatBoxElement.append(messageElement);
+
+      const allMessageElements = document.querySelectorAll(".chat-strip p");
+
+      if (allMessageElements.length > MAX_MESSAGES_COUNT) {
+        // remove the first message from the beginning of the list
+        allMessageElements[0]?.remove();
+      }
+    });
+  } else {
+    document.querySelector(".chat-strip").style.display = "none";
+
+    client.on("message", async (channel, tags, message, self) => {
+      const messageElement = await createMessageElement(tags, message);
+
+      const chatBoxElement = document.querySelector(".chat-box");
+      chatBoxElement.append(messageElement);
+
+      const allMessageElements = document.querySelectorAll(".chat-box p");
+
+      if (allMessageElements.length > MAX_MESSAGES_COUNT) {
+        // remove the first message from the beginning of the list
+        allMessageElements[0]?.remove();
+      }
+    });
+  }
 }
+
+document.addEventListener("DOMContentLoaded", setup);
