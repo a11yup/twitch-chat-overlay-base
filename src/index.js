b16a32acdf3b22623a88640c67e618af3bb128ac
A11y Up <a11yup+github@posteo.de>
2022-07-08 03:05:46 +0200

    Simplify setup code

--- src/index.js
+++ src/index.js
@@ -7,45 +7,37 @@ function setup() {
   const CHANNEL_NAME = queryParameters.get("channel");
   const CHAT_STRIP_MODE = queryParameters.has("chatStrip");
 
+  let chatContainerSelectorName;
+
+  if (CHAT_STRIP_MODE) {
+    document.querySelector(".chat-box").style.display = "none";
+    chatContainerSelectorName = ".chat-strip";
+  } else {
+    document.querySelector(".chat-strip").style.display = "none";
+    chatContainerSelectorName = ".chat-box";
+  }
+
   const client = tmi.Client({
     channels: [CHANNEL_NAME],
   });
 
   client.connect();
 
-  if (CHAT_STRIP_MODE) {
-    document.querySelector(".chat-box").style.display = "none";
+  client.on("message", async (channel, tags, message, self) => {
+    const messageElement = await createMessageElement(tags, message);
 
-    client.on("message", async (channel, tags, message, self) => {
-      const messageElement = await createMessageElement(tags, message);
+    const chatBoxElement = document.querySelector(chatContainerSelectorName);
+    chatBoxElement.append(messageElement);
 
-      const chatBoxElement = document.querySelector(".chat-strip");
-      chatBoxElement.append(messageElement);
+    const allMessageElements = document.querySelectorAll(
+      `${chatContainerSelectorName} p`
+    );
 
-      const allMessageElements = document.querySelectorAll(".chat-strip p");
-
-      if (allMessageElements.length > MAX_MESSAGES_COUNT) {
-        // remove the first message from the beginning of the list
-        allMessageElements[0]?.remove();
-      }
-    });
-  } else {
-    document.querySelector(".chat-strip").style.display = "none";
-
-    client.on("message", async (channel, tags, message, self) => {
-      const messageElement = await createMessageElement(tags, message);
-
-      const chatBoxElement = document.querySelector(".chat-box");
-      chatBoxElement.append(messageElement);
-
-      const allMessageElements = document.querySelectorAll(".chat-box p");
-
-      if (allMessageElements.length > MAX_MESSAGES_COUNT) {
-        // remove the first message from the beginning of the list
-        allMessageElements[0]?.remove();
-      }
-    });
-  }
+    if (allMessageElements.length > MAX_MESSAGES_COUNT) {
+      // remove the first message from the beginning of the list
+      allMessageElements[0]?.remove();
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", setup);
